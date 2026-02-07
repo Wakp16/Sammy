63396534304a4326fe1deccec918961392696ffe
Wakp16 <151411768+Wakp16@users.noreply.github.com>
2026-02-07 10:21:32 +0800

    Upgrade service worker to version 2 with caching improvements

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const cacheName = "date-todo-v1";
+const cacheName = "date-todo-v2";
 const assetsToCache = [
+  "./",
   "./index.html",
   "./style.css",
   "./script.js",
   "./manifest.json",
+  "./texture.jpg",
   "./icon-192.png",
   "./icon-512.png"
 ];
@@ -12,17 +14,47 @@ const assetsToCache = [
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
 // Activate event
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== cacheName).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
 });
 
 // Fetch event
 self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(cacheName).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(networkResponse => {
+        const copy = networkResponse.clone();
+        caches.open(cacheName).then(cache => cache.put(event.request, copy));
+        return networkResponse;
+      });
+    })
   );
+});
+
+self.addEventListener("message", event => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
